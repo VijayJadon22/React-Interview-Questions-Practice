cf4479bfcbd02ba2d5a651aaf526e7c8c3558937
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-08 21:47:06 +0530

    Implemnted a task asked to show names in array after clicking on heading

--- index.js
+++ index.js
@@ -11,5 +11,22 @@
 // console.log(NaN === NaN);
 
 
-console.log(1 < 2 < 3);
-console.log(3 > 2 > 1);
+// console.log(1 < 2 < 3);
+// console.log(3 > 2 > 1);
+
+const arr = ["Aman", "Raghav", "Arun", "Shivam"];
+const heading = document.getElementById("greeting");
+// let index = 0;
+heading.addEventListener("click", () => {
+    // const random = Math.floor(Math.random() * 4);
+    // console.log(random);
+    // heading.innerText = `Hello ${arr[random]}`;
+
+    if (index == 4) { index = 0; }
+    console.log(index);
+    heading.innerText = `Hello ${arr[index]}`;
+    index++;
+})
+
+const random = Math.ceil(Math.random() * 4);
+console.log(random);
