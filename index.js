92a0bc6e07b916806f4d40714a58aacb0cc5d460
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-08 17:58:25 +0530

    Interesting question about comparision

--- index.js
+++ index.js
@@ -6,6 +6,10 @@
 // console.log(5 == '5'); // so basically first double equal to operator checks only for valye so true
 // console.log(5 === '5'); // so basically triple equal to operator checks value as well as data type so false
 
-// console.log(typeof (NaN));
 
-console.log(NaN === NaN);
+// console.log(typeof (NaN));
+// console.log(NaN === NaN);
+
+
+console.log(1 < 2 < 3);
+console.log(3 > 2 > 1);
