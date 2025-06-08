4aeab03b222a7b6232974451fe3dd30704523e6a
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-08 16:59:03 +0530

    Asked difference between comparision operators

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // console.log('5' + 3); //53
-const a = '5' + 3;
-console.log(a + typeof (a));
-console.log('5' - 3); //2
+// const a = '5' + 3;
+// console.log(a + typeof (a));
+// console.log('5' - 3); //2
+
+console.log(5 == '5'); // so basically first double equal to operator checks only for valye so true
+console.log(5 === '5'); // so basically triple equal to operator checks value as well as data type so false
